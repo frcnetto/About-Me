0f66a1e6fa4228fb450e89bbb9a9d9246c3ab997
Netto Cavalcante <frcnetto@gmail.com>
2019-05-26 17:43:48 -0300

    Adding databind and finishing the app

--- app/src/main/java/br/com/frcnetto/aboutme/MainActivity.kt
+++ app/src/main/java/br/com/frcnetto/aboutme/MainActivity.kt
@@ -1,12 +1,47 @@
 package br.com.frcnetto.aboutme
 
+import android.content.Context
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import br.com.frcnetto.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName = MyName("Netto")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener { addNickname(it) }
+    }
+
+    private fun addNickname(v: View){
+
+        binding.apply {
+            myName?.nickName = nicknameEdit.text.toString()
+            invalidateAll()
+            changeVisibility(nicknameEdit, View.GONE)
+            changeVisibility(v, View.GONE)
+            changeVisibility(nicknameText, View.VISIBLE)
+        }
+
+        hideKeyboard(v)
+    }
+
+    private fun changeVisibility(view: View, visibility: Int) {
+        view.visibility = visibility
+    }
+
+    private fun hideKeyboard(view: View){
+        val inputMethodManager =  getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
---
+++ app/src/main/java/br/com/frcnetto/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package br.com.frcnetto.aboutme
+
+data class MyName (var name : String = "", var nickName : String = "")
